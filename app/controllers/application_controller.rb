3ed5c7a5bb3f8cefde4412a296d2da519c09e3c7
Shannon Coyne <scoyne@wellesley.edu>
2015-05-11 12:17:50 -0400

    removed authenticate method [using before_filter instead]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  def authenticate
-    if !admin_signed_in?
-      redirect_to posts_path
-    end
-  end
 end
